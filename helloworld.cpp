7aa0e78ddebf3624574722b47438b0c98e0f98e0
Jocelyn Pacheco <72085984+jopacheco113@users.noreply.github.com>
2025-02-26 21:09:19 +0000

    added comments for readibility

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/* this is my first comment, and i will explain to you that this is my first c++ code
+the main function simply prints out some text and returns 0 on success */
+
 int main() {
-  cout << "hello world!\n";
-  cout << "this is another piece of text 0.o\n\n";
-  return 0;
+  cout << "hello world!\n"; //prints the words hello world! on their own line
+  cout << "this is another piece of text 0.o\n\n"; //prints the text on its own line, followed by an empty line
+  return 0; //returns code 0 on success
 }
