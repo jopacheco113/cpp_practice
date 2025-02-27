eac9b021a6f7d3a257ef02cc215515f7d6611dee
Jocelyn Pacheco <72085984+jopacheco113@users.noreply.github.com>
2025-02-27 14:17:52 -0500

    Create adventure.cpp

---
+++ adventure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/* this will be a text based choose-your-own-adventure game that will give some context and ask for user input. */
+
+int main() {
+  cout << "hello friend! are you ready for an adventure?";
+  //here is where i will put some code
+  return 0;
+}
