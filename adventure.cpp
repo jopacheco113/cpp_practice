6ea089582f612a19b716aa4a4bf380ee6a26a992
Jocelyn Pacheco <72085984+jopacheco113@users.noreply.github.com>
2025-02-27 14:23:57 -0500

    Update adventure.cpp
    added a rudimentary test of user input and response

--- adventure.cpp
+++ adventure.cpp
@@ -4,7 +4,10 @@ using namespace std;
 /* this will be a text based choose-your-own-adventure game that will give some context and ask for user input. */
 
 int main() {
-  cout << "hello friend! are you ready for an adventure?";
+  string userResponse;
+  cout << "hello friend! are you ready for an adventure?\n";
+  cin >> userResponse;
+  cout << "your response was: " << userResponse << "\n";
   //here is where i will put some code
   return 0;
 }
